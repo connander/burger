2c313517f9263a647610acdeaaab6e1c56f1c64c
connander <connorpstark@gmail.com>
2019-10-13 16:49:40 -0500

    created config files, db files, and started work of models files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
+INSERT INTO burgers (burger_name) VALUES ("Jiffy Burger");
